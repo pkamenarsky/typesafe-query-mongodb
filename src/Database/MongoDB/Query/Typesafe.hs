7305e843a781c987054c86cfc42b65157a3897f9
Philip Kamenarsky <p.kamenarsky@gmail.com>
2015-01-23 19:19:47 +0100

    Bson

--- src/Database/MongoDB/Query/Typesafe.hs
+++ src/Database/MongoDB/Query/Typesafe.hs
@@ -1,3 +1,15 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Database.MongoDB.Query.Typesafe where
 
-import Database.MongoDB.Query as Q
+import           Data.Bson
+
+import qualified Database.MongoDB.Query as DB
+import           TSQuery.Query
+
+query :: DB.Selector -> DB.Collection -> DB.Query
+query sel col = DB.Query [] (DB.Select sel col) [] 0 0 [] False 0 []
+
+tsQueryToQuery :: Query a -> DB.Query
+tsQueryToQuery QAll = query [] ""
+tsQueryToQuery (QEq (Entity fld) v) = query [fld := v] ""
